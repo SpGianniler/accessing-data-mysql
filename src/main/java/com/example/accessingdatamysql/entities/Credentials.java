f5fc7255bc66074a9e5be24bfb68f520c7903b28
SpGianniler <sp.gianniler@gmail.com>
2020-11-27 11:16:47 +0200

    -added Employees support

---
+++ src/main/java/com/example/accessingdatamysql/entities/Credentials.java
@@ -0,0 +1,4 @@
+package com.example.accessingdatamysql.entities;
+
+public class Credentials {
+}
--- src/main/java/com/example/accessingdatamysql/entities/Employees.java
+++ src/main/java/com/example/accessingdatamysql/entities/Employees.java
@@ -1,16 +1,19 @@
 package com.example.accessingdatamysql.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "employees")
+@Getter
+@Setter
 public class Employees {
     @Id
     @Column(name = "eid")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int eid;
 
     @Column(name = "first_name")
@@ -24,4 +27,44 @@ public class Employees {
 
     @Column(name = "jid")
     private int jid;
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public int getJid() {
+        return jid;
+    }
+
+    public void setJid(int jid) {
+        this.jid = jid;
+    }
 }
